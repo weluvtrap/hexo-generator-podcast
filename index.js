8e6e5e8b39fe3cdd228e7c2778b1e2f02ccf369c
Earl Ferguson <earl@ullisen.com>
2021-05-07 01:16:29 -0400

    Update rss2.xml

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 /* global hexo */
-'use strict';
+"use strict";
 
-var assign = require('object-assign');
-var pathFn = require('path');
+var assign = require("object-assign");
+var pathFn = require("path");
 
-var config = hexo.config.podcast = assign({
-  type: 'rss2',
-  limit: 20,
-  hub: '',
-  content: true
-}, hexo.config.podcast);
+var config = (hexo.config.podcast = assign(
+  {
+    type: "rss2",
+    limit: 20,
+    hub: "",
+    content: true,
+  },
+  hexo.config.podcast
+));
 
 var type = config.type.toLowerCase();
 
 // Set default feed path
 if (!config.path) {
-  config.path = config.type + '.xml';
+  config.path = config.type + ".xml";
 }
 
 // Add extension name if don't have
 if (!pathFn.extname(config.path)) {
-  config.path += '.xml';
+  config.path += ".xml";
 }
 
-hexo.extend.generator.register('podcast', require('./lib/generator'));
+hexo.extend.generator.register("podcast", require("./lib/generator"));
